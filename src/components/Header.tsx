b177fcbe4f1c064246c16c8e06454782412b9689
t4traw <t4traw@gmail.com>
2022-02-08 11:50:00 +0900

    ログイン/ログアウトを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import React, { Dispatch, SetStateAction } from 'react'
 import { List } from 'phosphor-react'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Header: React.FC<{ updateSidebarState: () => void }> = ({ updateSidebarState }) => {
+  const { data: session, status } = useSession()
+
   return (
     <div className="flex flex-row py-4">
       <div className="basis-1/3 flex items-center">
@@ -11,7 +14,15 @@ const Header: React.FC<{ updateSidebarState: () => void }> = ({ updateSidebarSta
         <img src="/images/logo.svg" height="35" width="35" className="h-10 mx-auto" />
       </div>
       <div className="basis-1/3 flex items-center justify-end">
-        <p className="text-right">アカウント作成</p>
+        {status === 'authenticated' ? (
+          <p className="text-right" onClick={() => signOut()}>
+            ログアウト
+          </p>
+        ) : (
+          <p className="text-right" onClick={() => signIn('google')}>
+            ログイン
+          </p>
+        )}
       </div>
     </div>
   )
